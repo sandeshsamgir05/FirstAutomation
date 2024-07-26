2e9844ff09944af5cfe7ef2cfc297a9316b57735
sandeshsamgir05 <sandesh.samgir05@gmail.com>
2024-07-26 11:34:01 +0530

    1st test

---
+++ src/test/java/day1/FirstTestCase.java
@@ -0,0 +1,30 @@
+package day1;
+
+
+
+import org.openqa.selenium.chrome.ChromeDriver;	
+
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.opencart.com/");
+		
+		String act_title = driver.getTitle();
+		if (act_title.equals("OpenCart - Open Source Shopping Cart Solution" ))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+				
+		driver.close();
+		
+	}
+
+}
